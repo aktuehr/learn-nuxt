c8d8a9be3d676ace6a326c5fa4693d38d1c45444
Akito UEHARA <akt.uehr@gmail.com>
2019-07-20 21:55:14 +0900

    feat: HeadTextコンポーネントを追加

---
+++ components/HeadText.vue
@@ -0,0 +1,39 @@
+<template>
+  <component
+    v-bind:is="headTagByHeadLebel({level})"
+    class="o_c-head-text"
+  >
+    {{ text }}
+  </component>
+</template>
+
+<style lang="scss" scoped>
+.o_c-head-text {
+  font-size: 1.8rem;
+  font-weight: bold;
+  color: #00bfff;
+}
+</style>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+export interface HeadTextProps {
+  text: string;
+  level?: string;
+}
+
+@Component
+export default class HeadText<HeadTextProps> extends Vue {
+  @Prop()
+  public text!: string;
+  @Prop({
+    default: '1'
+  })
+  public level?: string;
+
+  public headTagByHeadLebel({ level }) {
+    return `h${level}`;
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="container">
-
+    <section>
+      <h1>HeadTextコンポーネント</h1>
+      <HeadText text="ここはh1で指定" level="1" />
+      <HeadText text="ここはh2で指定" level="2" />
+      <HeadText text="ここはh3で指定" level="3" />
+      <HeadText text="ここはh4で指定" level="4" />
+      <HeadText text="ここはh5で指定" level="5" />
+      <HeadText text="ここは指定なし" />
+    </section>
   </div>
 </template>
 
 <script>
+import HeadText from '../components/HeadText.vue';
 
 export default {
   components: {
+    HeadText
   }
 }
 </script>
